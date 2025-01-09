e67fdf2645f6b53bad8c9047e5327552d0ac3a96
Maheen Sabahat <maheensabahat5@gmail.com>
2025-01-10 00:06:27 +0500

    Product of two integers in C++

---
+++ BBT243019 (1)/Code 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1, num2, product;
+    cout << "Enter the first integer: ";
+    cin >> num1;
+    cout << "Enter the second integer: ";
+    cin >> num2;
+    product = num1 * num2;
+    cout << "The product is " <<product<<"\n";
+
+    return 0;
+}
